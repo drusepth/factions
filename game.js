442fc07585425e809679698eb5a4717c4f5599b4
Andrew Brown <drusepth@gmail.com>
2017-10-21 01:15:13 +0300

    some basic piece selection

--- game.js
+++ game.js
@@ -3,3 +3,50 @@ var map = jsboard.board({
   attach: "game",
   size: "12x12"
 });
+
+
+
+
+
+
+
+
+
+
+// Pieces
+var x = jsboard.piece({
+  text: "X",
+  fontSize: "40px",
+  textAlign: "center"
+});
+var o = jsboard.piece({
+  text: "O",
+  fontSize: "40px",
+  textAlign: "center"
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// Logic
+
+var turn = true;
+map.cell("each").on("click", function() {
+  if (map.cell(this).get() === null) {
+    if (turn) { map.cell(this).place(x.clone()); }
+    else      { map.cell(this).place(o.clone()); }
+    turn = !turn;
+  } else {
+    map.cell(this).style({
+      background: "gold",
+    });
+  }
+});
